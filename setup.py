5a19b15bc672696de2695d7a69037052128436bb
Dave B <dave@bx.psu.edu>
2015-10-21 13:46:52 -0400

    Implement suggestions per Nate's feedback
    - Correct author.
    - Make sure setuptools is available.

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup, find_packages
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
 setup( name="galaxy_sequence_utils",
        version="1.0.0",
        packages=find_packages('lib'),
        package_dir={ '': 'lib' },
        setup_requires=[],
-       author="Dave Bouvier",
-       author_email="dave@bx.psu.edu",
+       author="Dan Blankenberg, Galaxy Team",
+       author_email="galaxy-dev@lists.bx.psu.edu",
        description="Galaxy utilities for manipulating sequences.",
-       url="http://galaxyproject.org",
+       url="https://github.com/galaxyproject/sequence_utils",
        zip_safe=False,
        dependency_links=[] )
