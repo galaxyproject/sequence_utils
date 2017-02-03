cee9469c2c4ad55b3b6a7c64dbd259f5ede59658
John Chilton <jmchilton@gmail.com>
2017-02-03 14:08:53 -0500

    Update setup.py to reflect Python 3 support now.

--- setup.py
+++ setup.py
@@ -81,6 +81,11 @@ setup(
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     test_suite=TEST_DIR,
     tests_require=test_requirements
